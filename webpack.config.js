2405b7964e1b829d3229d1980444895281c25c9d
Steven Moody <smoody07@gmail.com>
2022-09-21 15:02:28 -0700

    Updates webconfig for correct dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: "production",
   entry: path.join(__dirname, "client/src", "index.jsx"),
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "client/dist"),
   },
   module: {
     rules: [
